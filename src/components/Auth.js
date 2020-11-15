668385517655a19f08e476e96330ff1fc7b492c2
Sangsu Lee <63631278+Lee-Sangsu@users.noreply.github.com>
2020-11-15 22:16:31 +0900

    create form at Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => { 
+        const {
+            target: {
+                name, value
+            }
+        } = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+
     return (
     <>
         <span>로그인</span>
         <h2>계정을 만들고 산책로를 추가해보세요</h2>
         <form>
-            <input>ㅁ</input>
+            <h4>이메일</h4>
+            <input name="email" type="email" value="email" onChange={onChange} required></input>
+            <h4>비밀번호</h4>
+            <input name="password" type="password" value="password" onChange={onChange} required></input>
         </form>
     </>
     );
