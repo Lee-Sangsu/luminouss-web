69fcacd63f777dd6503c685b0205a5da55dc6ff1
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-11-21 20:08:14 +0900

    remove chakra in main branch

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box } from "@chakra-ui/core";
 
 const Home = ({ isLoggedIn }) => {
     return (
@@ -8,9 +7,8 @@ const Home = ({ isLoggedIn }) => {
             <h1>시각장애인의 눈과 발이 되어주세요.</h1>
             <h3>문제 설명 .....</h3>
 
-                <Box bg="black" w="100px" h="100px" maxW="sm" borderWidth="1px" rounded="lg" p={4} className="add-road-info" >
+
                     <Link to="/add-road-info" >산책로 추가하기</Link>
-                </Box>
         </>
     );
 };
