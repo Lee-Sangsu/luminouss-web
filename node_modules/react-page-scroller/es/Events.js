e82f40b89bfbb0be166b7e3ff91f272ce5b7c829
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-12-24 09:34:01 +0900

    fixed untracked files

--- node_modules/react-page-scroller/es/Events.js
+++
@@ -1,2 +0,0 @@
-export var TOUCHMOVE = "touchmove";
-export var KEYDOWN = "keydown";
--- node_modules/react-page-scroller/es/utils.js
+++
@@ -1,9 +0,0 @@
-export var isNil = function isNil(value) {
-  return value === undefined || value === null;
-};
-export var isNull = function isNull(value) {
-  return value === null;
-};
-export var isPositiveNumber = function isPositiveNumber(value) {
-  return value > 0;
-};
