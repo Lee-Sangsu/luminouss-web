c65298fc5f5026c8c1b8fdd0d903bef7ea4f7e7b
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2021-01-28 15:20:19 +0900

    edited EditRest part

--- src/components/molecules/ForEditRoad/EditRest.js
+++ src/components/molecules/ForEditRoad/EditRest.js
@@ -24,14 +24,14 @@ const EditRest = ({setToilet, setBench}) => {
       setToilet('없었다.');
       document.getElementById("first-toilet").style.display='none';
     } else if (event.target.name==="second-yes"){
-      setMQuestion(true);
+      setMQuestion(false);
       setToiletExist(false);
       document.getElementById("first-toilet").style.display='none';
       document.getElementById('toilet-input').style.display = 'block';
       document.getElementById('second-toilet').style.display = 'none';
       document.getElementById('toilet-yes-no').style.display = 'none';
     }  else if (event.target.name==="second-no"){
-      setMQuestion(false);
+      setMQuestion(true);
       setToiletExist(false);
       document.getElementById("first-toilet").style.display='none';
       document.getElementById('toilet-input').style.display = 'block';
@@ -45,14 +45,14 @@ const EditRest = ({setToilet, setBench}) => {
       setBenchExist(false);
       setBench('없었다.');
     } else if (event.target.name==="second-b-yes"){
-      setBenchMQuestion(true);
+      setBenchMQuestion(false);
       document.getElementById("first-bench").style.display='none';
       document.getElementById('bench-input').style.display = 'block';
       document.getElementById('second-bench').style.display = 'none';
       document.getElementById('bench-yes-no').style.display = 'none';
 
     } else if (event.target.name==="second-b-no"){
-      setBenchMQuestion(false);
+      setBenchMQuestion(true);
       document.getElementById("first-bench").style.display='none';
       document.getElementById('bench-input').style.display = 'block';
       document.getElementById('second-bench').style.display = 'none';
@@ -95,7 +95,7 @@ const EditRest = ({setToilet, setBench}) => {
           </div>  
           : <div id='toilet-input'>
               <label id="toilet-q">
-                화장실이<input type="number" style={{margin:'0 10px'}} name="toilet" value={toiletNum} onChange={onChange} /> 곳 있었다.  
+                화장실이<input type="number" style={{margin:'0 10px'}} name="toilet-q" value={toiletNum} onChange={onChange} /> 곳 있었다.  
               </label>
             </div>  }  
       
@@ -106,7 +106,7 @@ const EditRest = ({setToilet, setBench}) => {
       </div>
       <div id="first-bench">
           <div className="questions-form" >
-            <h3 className='question'>산책로에 벤치와 같은 휴식 공간은 얼마나 있었나요?</h3>
+            <h3 className='question'>산책로에 벤치와 같은 휴식 공간이 있었나요?</h3>
             <input className="yes-input" type="radio" value="yes" name="first-b-yes" checked={benchIsExist === true} onChange={onChange} />
             <input type="radio" value="no" name="first-b-no" checked={benchIsExist === false} onChange={onChange} /> 
           </div>
