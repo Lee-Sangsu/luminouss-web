c5192897e29f6d783fe690670d827eb12ab6efed
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2021-05-31 14:20:20 +0900

    popup img changed:

--- src/global/App.js
+++ src/global/App.js
@@ -3,11 +3,15 @@ import AppRouter from './Router';
 import firebase from './fbase';
 import {useRecoilState} from 'recoil';
 import InitializeState from 'recoilStates/InitializeState';
-
+import ReactGA from 'react-ga';
 
 function App() {
   const [init, setInit] = useRecoilState(InitializeState);
-
+  
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_MEASUREMENT_ID);
+    ReactGA.pageview("/");
+  }, [])
 
   useEffect((() => {
     firebase.auth().onAuthStateChanged((user) => {
